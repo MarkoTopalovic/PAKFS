6e8e94fb951614cf7179b927e96813b09cb5fff3
zile <miroslav.zivkovic.mfkg@gmail.com>
2018-04-21 17:41:47 +0200

    dodati busarcevi fajlovi

---
+++ PAKS/pakzmod.f90
@@ -0,0 +1,34 @@
+      MODULE DRAKCE8
+        INTEGER*8 nwk8
+        INTEGER*8, DIMENSION(:), ALLOCATABLE :: MAXA8
+        INTEGER*4, DIMENSION(:), ALLOCATABLE :: IVRS
+        INTEGER*4, DIMENSION(:), ALLOCATABLE :: ISK
+        INTEGER*4, DIMENSION(:), ALLOCATABLE :: AIROWS
+      END MODULE
+      
+      MODULE FSIDENT
+        INTEGER*4, DIMENSION(:,:), ALLOCATABLE :: IDENT
+      END MODULE
+      
+      MODULE PLAST3D
+        REAL*8, DIMENSION(:), ALLOCATABLE :: PLAST
+        REAL*8, DIMENSION(:), ALLOCATABLE :: PLAS1
+        REAL*8, DIMENSION(:), ALLOCATABLE :: PLAS0
+      END MODULE
+      
+      MODULE ELEMENTI
+        INTEGER*4, DIMENSION(:,:), ALLOCATABLE :: VTKELEMENTI
+      END MODULE
+!     ovo sluzi za citanje / pisanje mesto fajla ali samo za 1 slucaj koji je pravio problem      
+      MODULE WSTAZK
+        LOGICAL ALLOCIRANAMATRICA
+        REAL*8, DIMENSION(:), ALLOCATABLE :: RTWRITE
+      END MODULE
+!     ovo je novi modul za cuvanje matrice K      
+      MODULE MATRICA
+        LOGICAL ALLOCIRANAMATRICAK
+        REAL*8, DIMENSION(:), ALLOCATABLE :: ALSK
+        REAL*8, DIMENSION(:), ALLOCATABLE :: ALSM
+        REAL*8, DIMENSION(:), ALLOCATABLE :: ALSC
+      END MODULE
+      
